a39a919a774310c119e9ba9a01585ee7e8e5e613
root <root@ip-172-31-24-26.us-west-2.compute.internal>
2020-02-24 23:06:12 +0000

    added my student id

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,9 +1,9 @@
 from __future__ import print_function
 import random
 
-buzz = ('continuous testing', 'continuous integration',
+buzz = ('Leeseul Kim', 'continuous testing', 'continuous integration',
     'continuous deployment', 'continuous improvement', 'devops')
-adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
+adjectives = ('A01029289', 'complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
